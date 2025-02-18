9bff0a164cb0d17290ca6261c2d1edf787d0ebef
NadeeM <nadeem.elbarbari@gmail.com>
2025-02-19 00:57:55 +0200

    crushing some bugs

--- index.js
+++ index.js
@@ -9,6 +9,9 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.use(
     cors({
         origin: ['http://localhost:5500', 'https://notes-app-fullstack-psi.vercel.app/'],
@@ -18,15 +21,9 @@ app.use(
     })
 );
 
-app.use(express.static(path.join(__dirname, 'public')));
-
 // app.get('/', (req, res) => {
 //     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 // });
-
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
 
 appController(app, express);
 
