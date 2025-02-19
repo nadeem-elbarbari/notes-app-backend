5af832d479234a07755e4a54098c226186f914e3
NadeeM <nadeem.elbarbari@gmail.com>
2025-02-19 02:40:34 +0200

    enhancing code

--- controllers/users/user.controller.js
+++ controllers/users/user.controller.js
@@ -6,5 +6,5 @@ import { authenticate } from '../../middleware/auth/auth.middleware.js';
 const router = Router();
 router.post('/signup', validation(validate.signupSchema), userService.signup);
 router.post('/login', validation(validate.signinSchema), userService.signin);
-router.get('/checktoken', authenticate, userService.checkToken);
+router.get('/auth/checktoken', authenticate, userService.checkToken);
 export default router;
